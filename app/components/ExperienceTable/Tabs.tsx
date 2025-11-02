308da6b64a507ca178bf169a8be437d81c80f0d1
sdewhitt <seth_dewhitt@outlook.com>
2025-11-01 23:13:53 -0400

    fixing light theme ui

--- app/components/ExperienceTable/Tabs.tsx
+++ app/components/ExperienceTable/Tabs.tsx
@@ -14,7 +14,7 @@ const TabsList = React.forwardRef<
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "relative z-10 flex w-fit items-center justify-start gap-1 rounded-t-xl rounded-b-none border border-border border-b-0 bg-stone-950 px-1.5 py-1 -mb-px after:content-[''] after:absolute after:-left-px after:top-full after:h-3 after:w-px after:bg-border before:content-[''] before:absolute before:left-0 before:top-full before:h-3.5 before:w-15 before:bg-stone-950",
+      "relative z-10 flex w-fit items-center justify-start gap-1 rounded-t-xl rounded-b-none border border-border border-b-0 bg-background px-1.5 py-1 -mb-px after:content-[''] after:absolute after:-left-px after:top-full after:h-3 after:w-px after:bg-border before:content-[''] before:absolute before:left-0 before:top-full before:h-3.5 before:w-15 before:bg-background",
       className,
     )}
     {...props}
@@ -30,10 +30,10 @@ const TabsTrigger = React.forwardRef<
     ref={ref}
     className={cn(
       `relative inline-flex h-9 items-center justify-center whitespace-nowrap rounded-md px-3 md:px-4 text-sm md:text-base font-medium transition duration-200 ease-out
-       focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-white/60 disabled:pointer-events-none disabled:opacity-50 
-       bg-transparent text-white/90 hover:text-white 
-       data-[state=active]:text-white data-[state=active]:after:content-[''] data-[state=active]:after:absolute data-[state=active]:after:left-1 data-[state=active]:after:right-1 data-[state=active]:after:-bottom-px data-[state=active]:after:h-px
-     data-[state=active]:after:bg-yellow-400`,
+       focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 
+       bg-transparent text-foreground opacity-90 hover:opacity-100 
+       data-[state=active]:text-foreground data-[state=active]:after:content-[''] data-[state=active]:after:absolute data-[state=active]:after:left-1 data-[state=active]:after:right-1 data-[state=active]:after:-bottom-px data-[state=active]:after:h-px
+     data-[state=active]:after:bg-primary`,
       className,
     )}
     {...props}
